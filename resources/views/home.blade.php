684f7131bed4b775c22683a47548ce57cf259c48
maulanakevinp <maulanakevinp@gmail.com>
2020-03-19 08:32:12 +0700

    update home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,18 @@
-@extends('layouts.app')
+@extends('layouts.welcome')
 
+@section('title')
+    {{ config('app.name') }}
+@endsection
 @section('content')
-<div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+        <div class="col-lg-5 col-md-7">
+            <div class="card bg-secondary shadow border-0">
+                <div class="card-body px-lg-5 py-lg-5">
+                    <h1 class="text-center">
+                        Selamat datang di UD. Special
+                    </h1>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
