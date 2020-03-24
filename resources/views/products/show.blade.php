2cd2eddc96ca8361925a77596868ad24f65f3a71
maulanakevinp <maulanakevinp@gmail.com>
2020-03-24 18:43:49 +0700

    update show.blade.php

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -44,10 +44,33 @@ Detail Produk {{ $product->nama }}
                         <div class="card-body">
                             <div class="row">
                                 <div class="col">
+                                    <h5 class="card-title text-uppercase text-muted mb-0">Total Permintaan</h5>
+                                    <span class="h2 font-weight-bold mb-0">
+                                        @if ($product->permintaan)
+                                            {{ $product->permintaan }}
+                                        @else
+                                            0
+                                        @endif
+                                    </span>
+                                </div>
+                                <div class="col-auto">
+                                    <div class="icon icon-shape bg-yellow text-white rounded-circle shadow">
+                                        <i class="ni ni-bag-17"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-6">
+                    <div class="card card-stats mb-4 mb-xl-0">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col">
                                     <h5 class="card-title text-uppercase text-muted mb-0">Total Produksi</h5>
                                     <span class="h2 font-weight-bold mb-0">
                                         @if ($product->produksi)
-                                            {{ $product->prodoksi }}
+                                            {{ $product->produksi }}
                                         @else
                                             0
                                         @endif
